0293f23f7add53fa1e020206df8a6b03cb462741
agustinn-dev <agustinpenelas@gmail.com>
2025-02-13 22:34:44 -0300

    Criando a CarHub Dealership

---
+++ android/app/src/main/kotlin/com/example/dealership_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dealership_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+const dummyCategories = [
+  Category(
+    id: 'c1',
+    title: 'Veículos Novos',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Veículos Usados',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Veículos Semi-Novos',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'SUVs',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Pick-ups',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Esportivos',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Veículos de Luxo',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Peças e Acessórios',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Serviço e Manutenção',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Test Drive',
+    color: Colors.teal,
+  ),
+];
