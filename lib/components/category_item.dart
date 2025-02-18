ebabff89ffcfbe145ad327b59f9db957ea5fb859
agustinn-dev <agustinpenelas@gmail.com>
2025-02-17 23:13:50 -0300

    Ajustando bordas e espaçamentos das categorias do App

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -9,7 +9,17 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(category.title),
+      padding: EdgeInsets.all(15),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            category.color.withValues(alpha: 128),
+            category.color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
     );
   }
 }
