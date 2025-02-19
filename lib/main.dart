fa62e604cfefc2385a715ae9a645b6760047f3ed
agustinn-dev <agustinpenelas@gmail.com>
2025-02-18 23:09:31 -0300

    Criando as rotas nomeadas no app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +26,10 @@ class MyApp extends StatelessWidget {
               titleLarge: TextStyle(fontSize: 20, fontFamily: 'RobotoCondesed'),
             ),
       ),
-      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME : (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS : (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
